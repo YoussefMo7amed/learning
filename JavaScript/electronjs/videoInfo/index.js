24de262636b41d90851ab2428c990c5c362914b8
youssefmo7amed <youssef.mo7amed@gmail.com>
2022-01-13 23:56:16 +0200

    first project: get video length

---
+++ JavaScript/electronjs/videoInfo/index.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const {app, BrowserWindow, ipcMain} = electron;
+const ffmpeg = require('fluent-ffmpeg');
+// ready is a preset string (event) that we know that the app is going to issue
+// Note that this just the app not the window.
+let mainWindow;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+// first arg has some data about the process that sent this event, you can got dive separate windows inside of our app and any one of them could trigger this event, so you know that from event object
+// second arg is the data
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        duration = metadata.format.duration
+        mainWindow.webContents.send('video:duration', metadata.format.duration)
+    });
+});
