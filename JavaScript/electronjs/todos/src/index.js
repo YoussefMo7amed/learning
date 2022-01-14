66416b46981d7520bbab9902f25d60be038b2e44
youssefmo7amed <youssef.mo7amed@gmail.com>
2022-01-14 23:53:30 +0200

    finish todos application

--- JavaScript/electronjs/todos/src/index.js
+++ JavaScript/electronjs/todos/src/index.js
@@ -1,23 +1,67 @@
 const electron = require("electron");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 app.on("ready", () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
     mainWindow.loadURL(`file://${__dirname}/main.html`);
     // building menu
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     // we have to add this function to put in use this menu
     Menu.setApplicationMenu(mainMenu);
+    // so if the user closed the main window, the whole app closes.
+    mainWindow.on("closed", () => {
+        app.quit();
+    });
 });
 
+ipcMain.on("input:add", (event, data) => {
+    mainWindow.webContents.send("input:add", data);
+    addWindow.close();
+});
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: "add new todo",
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on("close", () => (addWindow = null));
+}
+function clearTodos() {
+    mainWindow.webContents.send("todos:clear");
+}
 // Comment (1)
 const menuTemplate = [
     {
         label: "File",
         submenu: [
-            { label: "New Todo" },
+            {
+                label: "New Todo",
+                click() {
+                    createAddWindow();
+                },
+                accelerator: "CmdOrCtrl" + "+N",
+            },
+            {
+                label: "Clear Todos",
+                click() {
+                    clearTodos();
+                },
+                accelerator: "CmdOrCtrl" + "+D",
+            },
             {
                 label: "Quit",
                 accelerator: "CmdOrCtrl" + "+Q",
@@ -33,6 +77,24 @@ const menuTemplate = [
 // mac, ios, and all apple related OSs based on 'darwin'
 if (process.platform === "darwin") {
     menuTemplate.unshift({}); // add empty object at first
+}
+
+if (process.env.NODE_ENV !== "producation") {
+    menuTemplate.push({
+        label: "View",
+        submenu: [
+            {
+                role: "reload",
+            },
+            {
+                label: "Toggle Developer Tools",
+                accelerator: "CmdorCtrl" + "+Shift+I",
+                click(item, focusWindow) {
+                    focusWindow.toggleDevTools();
+                },
+            },
+        ],
+    });
 }
 
 /*
