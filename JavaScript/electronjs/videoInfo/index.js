cf0b4f84e2b2add4432a31f512959a2aa4714e33
Youssef Mohamed <39943970+YoussefMo7amed@users.noreply.github.com>
2022-01-14 13:44:41 +0200

    Update index.js

--- JavaScript/electronjs/videoInfo/index.js
+++ JavaScript/electronjs/videoInfo/index.js
@@ -8,6 +8,7 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({
         // I added this object because of nodeIntegration is false by default (true WAS the default)
         // this object for writting nodejs code inside HTML
+        // but in best practices try to avoid these because it uses nodejs it self (can access and do anything nodejs can)
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
