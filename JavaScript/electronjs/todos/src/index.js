a2418b32074039aa8896dfc9bbc211fecaaaa0a8
youssefmo7amed <youssef.mo7amed@gmail.com>
2022-01-14 18:50:52 +0200

    create menu bar

---
+++ JavaScript/electronjs/todos/src/index.js
@@ -0,0 +1,50 @@
+const electron = require("electron");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    // building menu
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    // we have to add this function to put in use this menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// Comment (1)
+const menuTemplate = [
+    {
+        label: "File",
+        submenu: [
+            { label: "New Todo" },
+            {
+                label: "Quit",
+                accelerator: "CmdOrCtrl" + "+Q",
+                click() {
+                    app.quit();
+                },
+            },
+        ],
+    },
+];
+
+// Comment (2)
+// mac, ios, and all apple related OSs based on 'darwin'
+if (process.platform === "darwin") {
+    menuTemplate.unshift({}); // add empty object at first
+}
+
+/*
+## Big Comments Section
+    1)
+    each menu that we add to this menu template corresponds to a single drop down on the status bar on the top
+    vscode for example has 8 objects [file, edit, selection, view, go, run, terminal, help]
+    every object has at least one property (which is label)
+
+    2)
+    - there is a problem with mac, which it will show first label as the app name (electron),
+    while in windows and linux it will normaly show app name and first label (File)
+    - we can add first object in array as empty object, that works for mac but not the others!
+    so we can twick it by know the OS and if it is mac we add empty object at first
+*/
